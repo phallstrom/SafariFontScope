748ebc7b549c993c47d923aad585cd0ae0539f24
Philip Hallstrom <philip@pjkh.com>
2014-03-02 12:11:36 -0800

    merge font-size and line-height

--- FontScope.safariextension/end-script.js
+++ FontScope.safariextension/end-script.js
@@ -1,17 +1,18 @@
 function handleContextMenu(event) {
 
-  t = document.defaultView.getComputedStyle(event.target, '');
-
-  color = t.getPropertyCSSValue('color');
-  r = parseInt(color.getRGBColorValue().red.cssText).toString(16);
-  g = parseInt(color.getRGBColorValue().green.cssText).toString(16);
-  b = parseInt(color.getRGBColorValue().blue.cssText).toString(16);
+  var t           = document.defaultView.getComputedStyle(event.target, '');
+  var color       = t.getPropertyCSSValue('color');
+  var r           = parseInt(color.getRGBColorValue().red.cssText).toString(16);
+  var g           = parseInt(color.getRGBColorValue().green.cssText).toString(16);
+  var b           = parseInt(color.getRGBColorValue().blue.cssText).toString(16);
+  var font_family = t.getPropertyCSSValue('font-family').cssText;
+  var font_size   = t.getPropertyCSSValue('font-size').cssText;
+  var line_height = t.getPropertyCSSValue('line-height').cssText;
 
   safari.self.tab.setContextMenuEventUserInfo(event, 
     {
-      'Font Family': t.getPropertyCSSValue('font-family').cssText,
-      'Font Size': t.getPropertyCSSValue('font-size').cssText,
-      'Line Height': t.getPropertyCSSValue('line-height').cssText,
+      'Font Family': font-family,
+      'Font Size': (font-size + ' / ' + line_height),
       'Font Color': ('#' + r + g + b).toUpperCase()
     }
   );
