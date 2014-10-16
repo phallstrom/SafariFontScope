0d3dad80adf1bebe80ebb9a1dcb2c8f33c78fed7
Philip Hallstrom <philip@pjkh.com>
2014-10-16 15:08:27 -0700

    rgb codes with zeros weren't displaying properly. fixes #3

--- FontScope.safariextension/end-script.js
+++ FontScope.safariextension/end-script.js
@@ -2,9 +2,9 @@ function handleContextMenu(event) {
 
   var t           = document.defaultView.getComputedStyle(event.target, '');
   var color       = t.getPropertyCSSValue('color');
-  var r           = parseInt(color.getRGBColorValue().red.cssText).toString(16);
-  var g           = parseInt(color.getRGBColorValue().green.cssText).toString(16);
-  var b           = parseInt(color.getRGBColorValue().blue.cssText).toString(16);
+  var r           = ('00' + parseInt(color.getRGBColorValue().red.cssText).toString(16)).slice(-2);
+  var g           = ('00' + parseInt(color.getRGBColorValue().green.cssText).toString(16)).slice(-2);
+  var b           = ('00' + parseInt(color.getRGBColorValue().blue.cssText).toString(16)).slice(-2);
   var font_family = t.getPropertyCSSValue('font-family').cssText;
   var font_size   = t.getPropertyCSSValue('font-size').cssText;
   var font_weight = t.getPropertyCSSValue('font-weight').cssText;
