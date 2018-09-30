6e4d5e11cb10611062f3e2f92fe93495908ca0dc
Philip Hallstrom <philip@pjkh.com>
2018-09-30 11:23:27 -0700

    add rgba alpha channel to color and opacity

--- FontScope.safariextension/end-script.js
+++ FontScope.safariextension/end-script.js
@@ -1,25 +1,31 @@
 function handleContextMenu(event) {
 
-  var t           = document.defaultView.getComputedStyle(event.target, '');
-  var color       = t.getPropertyCSSValue('color');
-  var r           = ('00' + parseInt(color.getRGBColorValue().red.cssText).toString(16)).slice(-2);
-  var g           = ('00' + parseInt(color.getRGBColorValue().green.cssText).toString(16)).slice(-2);
-  var b           = ('00' + parseInt(color.getRGBColorValue().blue.cssText).toString(16)).slice(-2);
-  var font_family = t.getPropertyCSSValue('font-family').cssText;
-  var font_size   = t.getPropertyCSSValue('font-size').cssText;
-  var font_weight = t.getPropertyCSSValue('font-weight').cssText;
-  var line_height = t.getPropertyCSSValue('line-height').cssText;
+  const t           = document.defaultView.getComputedStyle(event.target, '');
+  const color       = t.getPropertyCSSValue('color').getRGBColorValue();
+  const r           = ('00' + parseInt(color.red.cssText).toString(16)).slice(-2);
+  const g           = ('00' + parseInt(color.green.cssText).toString(16)).slice(-2);
+  const b           = ('00' + parseInt(color.blue.cssText).toString(16)).slice(-2);
+  const a           = ('00' + parseInt(parseFloat(color.alpha.cssText) * 255).toString(16)).slice(-2);
+  const font_family = t.getPropertyCSSValue('font-family').cssText;
+  const font_size   = t.getPropertyCSSValue('font-size').cssText;
+  const font_weight = t.getPropertyCSSValue('font-weight').cssText;
+  const line_height = t.getPropertyCSSValue('line-height').cssText;
+  const opacity     = t.getPropertyCSSValue('opacity').cssText;
 
-  safari.self.tab.setContextMenuEventUserInfo(event, 
-    {
-      'Family': font_family,
-      'Size': (font_size + ' / ' + line_height),
-      'Weight': font_weight,
-      'Color': ('#' + r + g + b).toUpperCase()
-    }
-  );
+  var rgba  = ('#' + r + g + b);
+  if (a != 'ff') { rgba += a }
+
+  var output = {
+    'Family': font_family,
+    'Size': (font_size + ' / ' + line_height),
+    'Weight': font_weight,
+    'Color': rgba.toUpperCase(),
+  };
+
+  if (opacity != "1") { output['Opacity'] = opacity }
+
+  safari.self.tab.setContextMenuEventUserInfo(event, output);
 
 };
 
 document.addEventListener("contextmenu", handleContextMenu, false);
-
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 A Safari extension to easily view an element's CSS font properties via right click.
 
-Right click anywhere on the page and FontScope will display the font family, font size (and line height), and font color for the element under the mouse.
+Right click anywhere on the page and FontScope will display the font family, font size (and line height), font color, and opacity for the element under the mouse.
 
 ![Screenshot](https://raw.github.com/phallstrom/SafariFontScope/master/screenshot.png)
 
